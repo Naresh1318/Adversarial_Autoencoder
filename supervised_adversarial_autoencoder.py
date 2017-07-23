2e48f7ee377151b0733185fcbb8cc53262b72284
naresh1318 <rnaresh.n@gmail.com>
2017-07-23 14:26:08 +0530

    initial commit

---
+++ supervised_adversarial_autoencoder.py
@@ -0,0 +1,211 @@
+import tensorflow as tf
+import numpy as np
+import datetime
+import os
+import progressbar
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+from tensorflow.examples.tutorials.mnist import input_data
+
+# Progressbar
+bar = progressbar.ProgressBar(widgets=['[', progressbar.Timer(), ']', progressbar.Bar(), '(', progressbar.ETA(), ')'])
+
+# Get the MNIST data
+mnist = input_data.read_data_sets('./Data', one_hot=True)
+
+# Parameters
+input_dim = 784
+n_l1 = 1000
+n_l2 = 1000
+z_dim = 15
+batch_size = 100
+n_epochs = 1000
+learning_rate = 0.001
+beta1 = 0.9
+results_path = './Results'
+n_labels = 10
+
+# Placeholders for input data and the targets
+x_input = tf.placeholder(dtype=tf.float32, shape=[batch_size, input_dim], name='Input')
+y_input = tf.placeholder(dtype=tf.float32, shape=[batch_size, n_labels], name='Labels')
+x_target = tf.placeholder(dtype=tf.float32, shape=[batch_size, input_dim], name='Target')
+real_distribution = tf.placeholder(dtype=tf.float32, shape=[batch_size, z_dim], name='Real_distribution')
+manual_decoder_input = tf.placeholder(dtype=tf.float32, shape=[1, z_dim + n_labels], name='Decoder_input')
+
+
+def form_results():
+    folder_name = "/{0}_{1}_{2}_{3}_{4}_{5}_V2". \
+        format(datetime.datetime.now(), z_dim, learning_rate, batch_size, n_epochs, beta1)
+    tensorboard_path = results_path + folder_name + '/Tensorboard'
+    saved_model_path = results_path + folder_name + '/Saved_models/'
+    log_path = results_path + folder_name + '/log'
+    if not os.path.exists(results_path + folder_name):
+        os.mkdir(results_path + folder_name)
+        os.mkdir(tensorboard_path)
+        os.mkdir(saved_model_path)
+        os.mkdir(log_path)
+    return tensorboard_path, saved_model_path, log_path
+
+
+def generate_image_grid(sess, op):
+    nx, ny = 10, 10
+    random_inputs = np.random.randn(10, z_dim) * 5.
+    sample_y = np.identity(10)
+    plt.subplot()
+    gs = gridspec.GridSpec(nx, ny, hspace=0.05, wspace=0.05)
+    i = 0
+    for r in random_inputs:
+        for t in sample_y:
+            r, t = np.reshape(r, (1, z_dim)), np.reshape(t, (1, n_labels))
+            dec_input = np.concatenate((t, r), 1)
+            x = sess.run(op, feed_dict={manual_decoder_input: dec_input})
+            ax = plt.subplot(gs[i])
+            i += 1
+            img = np.array(x.tolist()).reshape(28, 28)
+            ax.imshow(img, cmap='gray')
+            ax.set_xticks([])
+            ax.set_yticks([])
+            ax.set_aspect('auto')
+    plt.show()
+
+
+def dense(x, n1, n2, name):
+    with tf.variable_scope(name, reuse=None):
+        weights = tf.get_variable("weights", shape=[n1, n2],
+                                  initializer=tf.random_normal_initializer(mean=0., stddev=0.01))
+        bias = tf.get_variable("bias", shape=[n2], initializer=tf.constant_initializer(0.0))
+        out = tf.add(tf.matmul(x, weights), bias, name='matmul')
+        return out
+
+
+# The autoencoder network
+def encoder(x, reuse=False):
+    if reuse:
+        tf.get_variable_scope().reuse_variables()
+    with tf.name_scope('Encoder'):
+        e_dense_1 = tf.nn.relu(dense(x, input_dim, n_l1, 'e_dense_1'))
+        e_dense_2 = tf.nn.relu(dense(e_dense_1, n_l1, n_l2, 'e_dense_2'))
+        latent_variable = dense(e_dense_2, n_l2, z_dim, 'e_latent_variable')
+        return latent_variable
+
+
+def decoder(x, reuse=False):
+    if reuse:
+        tf.get_variable_scope().reuse_variables()
+    with tf.name_scope('Decoder'):
+        d_dense_1 = tf.nn.relu(dense(x, z_dim + n_labels, n_l2, 'd_dense_1'))
+        d_dense_2 = tf.nn.relu(dense(d_dense_1, n_l2, n_l1, 'd_dense_2'))
+        output = tf.nn.sigmoid(dense(d_dense_2, n_l1, input_dim, 'd_output'))
+        return output
+
+
+def discriminator(x, reuse=False):
+    if reuse:
+        tf.get_variable_scope().reuse_variables()
+    with tf.name_scope('Discriminator'):
+        dc_den1 = tf.nn.relu(dense(x, z_dim, n_l1, name='dc_den1'))
+        dc_den2 = tf.nn.relu(dense(dc_den1, n_l1, n_l2, name='dc_den2'))
+        output = dense(dc_den2, n_l2, 1, name='dc_output')
+        return output
+
+
+def train(train_model=True):
+    with tf.variable_scope(tf.get_variable_scope()):
+        encoder_output = encoder(x_input)
+        # Concat class label and the encoder output
+        decoder_input = tf.concat(1, [y_input, encoder_output])
+        decoder_output = decoder(decoder_input)
+
+    with tf.variable_scope(tf.get_variable_scope()):
+        d_real = discriminator(real_distribution)
+        d_fake = discriminator(encoder_output, reuse=True)
+
+    with tf.variable_scope(tf.get_variable_scope()):
+        decoder_image = decoder(manual_decoder_input, reuse=True)
+
+    # Autoencoder loss
+    autoencoder_loss = tf.reduce_mean(tf.square(x_target - decoder_output))
+
+    # Discrimminator Loss
+    dc_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(targets=tf.ones_like(d_real), logits=d_real))
+    dc_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(targets=tf.zeros_like(d_fake), logits=d_fake))
+    dc_loss = dc_loss_fake + dc_loss_real
+
+    # Generator loss
+    generator_loss = tf.reduce_mean(
+        tf.nn.sigmoid_cross_entropy_with_logits(targets=tf.ones_like(d_fake), logits=d_fake))
+
+    all_variables = tf.trainable_variables()
+    dc_var = [var for var in all_variables if 'dc_' in var.name]
+    en_var = [var for var in all_variables if 'e_' in var.name]
+
+    # Optimizers
+    autoencoder_optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate,
+                                                   beta1=beta1).minimize(autoencoder_loss)
+    discriminator_optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate,
+                                                     beta1=beta1).minimize(dc_loss, var_list=dc_var)
+    generator_optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate,
+                                                 beta1=beta1).minimize(generator_loss, var_list=en_var)
+
+    init = tf.global_variables_initializer()
+
+    # Reshape immages to display them
+    input_images = tf.reshape(x_input, [-1, 28, 28, 1])
+    generated_images = tf.reshape(decoder_output, [-1, 28, 28, 1])
+
+    # Tensorboard visualization
+    tf.summary.scalar(name='Autoencoder Loss', tensor=autoencoder_loss)
+    tf.summary.scalar(name='Discriminator Loss', tensor=dc_loss)
+    tf.summary.scalar(name='Generator Loss', tensor=generator_loss)
+    tf.summary.histogram(name='Encoder Distribution', values=encoder_output)
+    tf.summary.histogram(name='Real Distribution', values=real_distribution)
+    tf.summary.image(name='Input Images', tensor=input_images, max_outputs=10)
+    tf.summary.image(name='Generated Images', tensor=generated_images, max_outputs=10)
+    summary_op = tf.summary.merge_all()
+
+    # Saving the model
+    saver = tf.train.Saver()
+    step = 0
+    with tf.Session() as sess:
+        if train_model:
+            tensorboard_path, saved_model_path, log_path = form_results()
+            sess.run(init)
+            writer = tf.summary.FileWriter(logdir=tensorboard_path, graph=sess.graph)
+            for i in bar(range(n_epochs)):
+                n_batches = int(mnist.train.num_examples / batch_size)
+                print("------------------Epoch {}/{}------------------".format(i, n_epochs))
+                for b in range(1, n_batches + 1):
+                    z_real_dist = np.random.randn(batch_size, z_dim) * 5.
+                    batch_x, batch_y = mnist.train.next_batch(batch_size)
+                    sess.run(autoencoder_optimizer, feed_dict={x_input: batch_x, x_target: batch_x, y_input: batch_y})
+                    sess.run(discriminator_optimizer,
+                             feed_dict={x_input: batch_x, x_target: batch_x, real_distribution: z_real_dist})
+                    sess.run(generator_optimizer, feed_dict={x_input: batch_x, x_target: batch_x})
+                    if b % 50 == 0:
+                        a_loss, d_loss, g_loss, summary = sess.run(
+                            [autoencoder_loss, dc_loss, generator_loss, summary_op],
+                            feed_dict={x_input: batch_x, x_target: batch_x,
+                                       real_distribution: z_real_dist, y_input: batch_y})
+                        writer.add_summary(summary, global_step=step)
+                        print("Epoch: {}, iteration: {}".format(i, b))
+                        print("Autoencoder Loss: {}".format(a_loss))
+                        print("Discriminator Loss: {}".format(d_loss))
+                        print("Generator Loss: {}".format(g_loss))
+                        with open(log_path + '/log.txt', 'a') as log:
+                            log.write("Epoch: {}, iteration: {}\n".format(i, b))
+                            log.write("Autoencoder Loss: {}\n".format(a_loss))
+                            log.write("Discriminator Loss: {}\n".format(d_loss))
+                            log.write("Generator Loss: {}\n".format(g_loss))
+                    step += 1
+
+                saver.save(sess, save_path=saved_model_path, global_step=step)
+        else:
+            # Get the latest results folder
+            all_results = os.listdir(results_path)
+            all_results.sort()
+            saver.restore(sess, save_path=tf.train.latest_checkpoint(results_path + '/' +
+                                                                     all_results[-1] + '/Saved_models/'))
+            generate_image_grid(sess, op=decoder_image)
+
+if __name__ == '__main__':
+    train(train_model=False)
